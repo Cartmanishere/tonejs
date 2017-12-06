a442595b017def4eee3f6e341c193a0cd2fbf630
Cartmanishere <apbetahouse45@gmail.com>
2017-12-06 16:42:19 +0530

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// middleware
+app.use(express.static('static'))
+
+app.get('/player', function(req, res){
+  res.sendFile(__dirname + '/static/player.html');
+});
+
+app.get('/controller', function(req, res){
+  res.sendFile(__dirname + '/static/controller.html');
+});
+
+io.on('connection', function(socket){
+    console.log('Connection received!')
+  // Received a connection
+  // Check if player or controller
+
+  // Setup a note transfer connection between controller and player
+    socket.on('controller', function(message){
+        io.emit('player', { note: message.note, duration: message.duration });
+    });
+
+
+
+
+
+    socket.on('disconnect', function(){
+       console.log('Someone disconnected.')
+    });
+});
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:'+process.env.PORT || 3000);
+});
